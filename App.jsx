db6befdee79d46693c83dd226a2bc17139922873
BablooS13 <babloosinghnmt123@gmail.com>
2026-05-28 18:07:33 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from 'react'
 import * as XLSX from 'xlsx'
 import accountsFileUrl from './assets/accounts.xlsx?url'
 
-const BASE_URL = 'https://api.plavix.skin/xxapi/buyitoken/waitpayerpaymentslip'
+const BASE_URL = 'https://invite.dawey.top/xxapi/buyitoken/waitpayerpaymentslip'
 const REQUEST_LIMIT = 200
 const TOKEN_STORAGE_KEY = 'floxpay-indiatoken'
 const OUTPUT_HEADERS = [
